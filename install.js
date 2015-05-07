c5841dd15aacda235f2f40b7c5bf8779367f5ed7
Tobias Lidskog <tobias@lidskog.se>
2015-05-07 12:19:01 +0200

    Download progress report, no functional changes.

--- install.js
+++ install.js
@@ -1,8 +1,10 @@
 'use strict';
 
-var Download = require('download');
+var Download = require('download'),
+    downloadStatus = require('download-status');
 
 new Download()
     .get('http://selenium-release.storage.googleapis.com/2.45/selenium-server-standalone-2.45.0.jar')
     .dest('vendor')
+    .use(downloadStatus())
     .run();
