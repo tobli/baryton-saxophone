9fd5c55ba415dee567ce780453debb3cf7c29934
Tobias Lidskog <tobias@lidskog.se>
2015-06-21 23:34:08 +0200

    Upgrade to Selenium jar version 2.46.

--- install.js
+++ install.js
@@ -4,7 +4,7 @@ var Download = require('download'),
     downloadStatus = require('download-status');
 
 new Download()
-    .get('http://selenium-release.storage.googleapis.com/2.45/selenium-server-standalone-2.45.0.jar')
+    .get('http://selenium-release.storage.googleapis.com/2.46/selenium-server-standalone-2.46.0.jar')
     .dest('vendor')
     .use(downloadStatus())
     .run();
