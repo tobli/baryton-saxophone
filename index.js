743c853ab5f8b80a822789d72ace25d781220094
Tobias Lidskog <tobias@lidskog.se>
2015-09-25 10:07:49 +0200

    Update to Selenium 2.47.1

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 var path = require('path');
 
 module.exports.jarPath = function() {
-  return path.resolve(__dirname, 'vendor', 'selenium-server-standalone-2.46.0.jar');
+  return path.resolve(__dirname, 'vendor', 'selenium-server-standalone.jar');
 };
--- install.js
+++ install.js
@@ -4,8 +4,9 @@ var Download = require('download'),
     downloadStatus = require('download-status');
 
 new Download()
-    .get('http://selenium-release.storage.googleapis.com/2.46/selenium-server-standalone-2.46.0.jar')
+    .get('http://selenium-release.storage.googleapis.com/2.47/selenium-server-standalone-2.47.1.jar')
     .dest('vendor')
+    .rename('selenium-server-standalone.jar')
     .use(downloadStatus())
     .run(function(err) {
       if (err) {
