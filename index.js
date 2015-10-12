d5b6b1d5a57ec0ac52fc127ff1bf204e82ba0e06
Tobias Lidskog <tobias@lidskog.se>
2015-10-12 13:42:52 +0200

    Adding api to get module version.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 
-var path = require('path');
+var path = require('path'),
+  version = require('./package').version;
+
+module.exports.version = version;
 
 module.exports.jarPath = function() {
   return path.resolve(__dirname, 'vendor', 'selenium-server-standalone.jar');
