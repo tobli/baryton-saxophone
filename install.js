7c97637fe6b00d3cf43a05c20cec4a2bd75deb54
Tobias Lidskog <tobias@lidskog.se>
2015-10-13 09:18:17 +0200

    Update to SeleniumServer 2.48.2.

--- install.js
+++ install.js
@@ -4,7 +4,7 @@ var Download = require('download'),
     downloadStatus = require('download-status');
 
 new Download()
-    .get('http://selenium-release.storage.googleapis.com/2.47/selenium-server-standalone-2.47.1.jar')
+    .get('http://selenium-release.storage.googleapis.com/2.48/selenium-server-standalone-2.48.2.jar')
     .dest('vendor')
     .rename('selenium-server-standalone.jar')
     .use(downloadStatus())
