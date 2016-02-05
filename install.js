117ef5510d82626c5e41f3c93fda1980dc9f18d6
Tobias Lidskog <tobias@lidskog.se>
2016-02-05 01:58:22 +0100

    Update to Selenium server 2.50.1.

--- install.js
+++ install.js
@@ -4,7 +4,7 @@ var Download = require('download'),
     downloadStatus = require('download-status');
 
 new Download()
-    .get('http://selenium-release.storage.googleapis.com/2.49/selenium-server-standalone-2.49.1.jar')
+    .get('http://selenium-release.storage.googleapis.com/2.50/selenium-server-standalone-2.50.1.jar')
     .dest('vendor')
     .rename('selenium-server-standalone.jar')
     .use(downloadStatus())
